e8114cd3f5b1ee98d920240721ecccaab8210948
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-21 14:43:40 -0300

    feat: Added auth provider to using context

--- web-react/src/main.tsx
+++ web-react/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { AuthProvider } from './contexts/auth';
+import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
