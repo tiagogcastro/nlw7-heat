84a8bddbcc2fe9d8528bafe28bf38a38bd81afe3
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-21 14:42:08 -0300

    feat: Added signin url using auth hook

--- web-react/src/components/LoginBox/index.tsx
+++ web-react/src/components/LoginBox/index.tsx
@@ -1,12 +1,16 @@
 import { VscGithubInverted } from 'react-icons/all';
 
+import { useAuth } from '../../hooks/useAuth';
+
 import styles from './styles.module.scss';
 
 export function LoginBox() {
+  const { signInUrl } = useAuth();
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size={24} />
         Entrar com Github
       </a>
