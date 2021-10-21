70dfc1c72747bb38be04751caec449f565ec75a3
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-21 13:44:40 -0300

    feat: Create component to listing messages

---
+++ web-react/src/components/MessageList/index.tsx
@@ -0,0 +1,46 @@
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg';
+
+export function MessageList() {
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/Tiaguin061.png" alt="Tiago Gonçalves" />
+            </div>
+            <span>Tiago Gonçalves</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/Tiaguin061.png" alt="Tiago Gonçalves" />
+            </div>
+            <span>Tiago Gonçalves</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/Tiaguin061.png" alt="Tiago Gonçalves" />
+            </div>
+            <span>Tiago Gonçalves</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
