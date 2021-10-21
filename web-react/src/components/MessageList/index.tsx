e349873cb1349b877399823d3d2d605267375aab
Tiago Gonçalves <tiaguin180@gmail.com>
2021-10-21 16:21:06 -0300

    feat: Added socket.io

--- web-react/src/components/MessageList/index.tsx
+++ web-react/src/components/MessageList/index.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
 import { api } from '../../services/api';
 
 import styles from './styles.module.scss';
@@ -16,12 +18,35 @@ type Message = {
   };
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:3333');
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
   useEffect(() => {
     api.get<Message[]>('/messages/last3').then(response => {
       setMessages(response?.data);
     })
+  }, []);
+
+  useEffect(() => {
+    setInterval(() => {
+      if(messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean));
+
+        messagesQueue.shift();
+      }
+    }, 3000);
   }, []);
   return (
     <div className={styles.messageListWrapper}>
